5b2dc52ddc7642ed15d7546e1282cc2beed3cec3
sinzitoyama <yamat422@gmail.com>
2022-12-16 13:58:21 +0900

    ゲームマネージャー追加

---
+++ Assets/Toyama/GameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool _start = false;
+
+    public bool _red = false;
+    public bool _white = false;
+    public bool _blue = false;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+
+    }
+    public void GameStart()
+    {
+        StartCoroutine(Hoge());
+        IEnumerator Hoge()
+        {
+            yield return new WaitUntil(() => Input.GetButtonDown("Jump"));
+            Debug.Log("Start");
+            _start = true;
+        }
+    }
+    public void GameOver()
+    {
+
+    }
+}
