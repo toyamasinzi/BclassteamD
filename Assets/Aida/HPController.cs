cf5ca78b58da4b796bbd4b177dab3990d54c0e28
Maya <mynhjm@icloud.com>
2022-12-19 21:07:43 +0900

    プレイヤーの色が変わる処理と敵を攻撃した時自分にダメージが加算される不具合を修正

--- Assets/Aida/HPController.cs
+++ Assets/Aida/HPController.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HPController : MonoBehaviour
 {
     [SerializeField] int _playerHP = 0;
+    [SerializeField] GameManager _gameManager;
+    [SerializeField] Text _Scoretext = default;
+
+    void Update()
+    {
+        _Scoretext.text = _playerHP.ToString("d2") + "%";
+    }
     public void Damage()
     {
         _playerHP += 10;
         if (_playerHP >= 100)
         {
-            GameObject gamemana = GameObject.Find("GameMnager");
-            
+            _gameManager.GameOver();
         }
     }
 }
